434fc5ff3c90f5026048a456a56eed35099484c7
hax0r <downtownxore@gmail.com>
2021-08-09 12:19:44 +0200

    AWS autodeploy scripty

--- tools/staging/environment.sh
+++ tools/staging/environment.sh
@@ -4,8 +4,13 @@ rm -rf /usr/bin/centos-shell &&
 rm -rf /usr/bin/impacket &&
 rm -rf /usr/bin/tomcat &&
 rm -rf /usr/bin/ubuntu-shell &&
+rm -rf /usr/bin/aws-deploy &&
+
+apt install awscli -y &&
+pip3 install boto3 &&
 
 link centos-shell.sh /usr/bin/centos-shell &&
 link impacket-smb-server.sh /usr/bin/impacket &&
 link tomcat.sh /usr/bin/tomcat &&
 link ubuntu-shell.sh /usr/bin/ubuntu-shell
+link aws-deploy.py /usr/bin/aws-deploy
