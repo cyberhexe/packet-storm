d6057ea97192f8d439cb67c3b7afb48e57ff1fac
totekuh <downtownxore@gmail.com>
2021-08-14 03:23:12 +0200

    removing old stuff

--- tools/active-information-gathering/connect-scan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <ip> <port-range>"
-    exit 0
-fi
-
-
-nc -nvv -w 1 -z $1 $2 2>/dev/null
--- tools/tunneling/rsync-remote.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -a remote_user@remote_host_or_ip:/opt/media/ /opt/media/
