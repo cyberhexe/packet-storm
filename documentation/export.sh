deb9dfbc1e90778fa126676e7c4a9609f3755851
totekuh <downtownxore@gmail.com>
2022-03-27 04:16:47 +0200

    docs tools

--- documentation/export.sh
+++ documentation/export.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-cherrytree_version="0.99.46"
-
 import_cherrytree_notebook="pentest-glossary.ctb"
 export_folder_name="glossary-export"
 
 echo "Installing cherrytree and xvfb"
+sudo add-apt-repository ppa:giuspen/ppa || exit 1
 sudo apt update || exit 1
-sudo apt install xvfb -y || exit 1
-wget "https://www.giuspen.com/software/cherrytree_$cherrytree_version.tar.xz" || exit 1
-sudo dpkg -i "cherrytree_$cherrytree_version.tar.xz" || exit 1
+sudo apt install xvfb cherrytree -y || exit 1
+
 
 echo "Cleaning old export folder"
 rm -rf "$export_folder_name"
